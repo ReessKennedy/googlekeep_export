cd98f483884a16d47a84db9368dc4ef1ee90fdd7
Reess Kennedy <reesskennedy2@gmail.com>
2023-09-06 22:30:06 -0400

    Update config_sample.py

--- config_sample.py
+++ config_sample.py
@@ -1,2 +1,15 @@
-username = "youremail@gmail.com"
-password = "__________"
+# GKEEP CONNECTION
+USERNAME = "youremail@gmail.com"
+PASSWORD = "app password"
+
+# CONFIG
+# Define the directory containing the exported JSON files
+JSON_DIRECTORY = 'keep_exported_json'
+
+# Define the output directory where downloaded images will be stored
+DOWNLOAD_FOLDER = '/www/wwwroot/files/'
+DOWNLOAD_URL = 'https://www.mydomain.com/files/' #Have to use a url below but would be better with a path
+
+
+
+
